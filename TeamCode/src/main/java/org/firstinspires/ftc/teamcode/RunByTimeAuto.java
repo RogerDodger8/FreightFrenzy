8965465c1e8c8cccb51ca0d400ca5c398d210ad0
Isabella6776 <bellagrayson06@gmail.com>
2021-11-08 16:38:34 -0700

    renamed auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunByTimeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RunByTimeAuto.java
@@ -55,7 +55,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Pushbot: Auto Drive By Time", group="Pushbot")
+@Autonomous(name="test auto", group="Pushbot")
 public class RunByTimeAuto extends LinearOpMode {
 
     /* Declare OpMode members. */
